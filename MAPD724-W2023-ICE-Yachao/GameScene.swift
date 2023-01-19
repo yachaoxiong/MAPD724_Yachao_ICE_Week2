cd7d24d55aef039ab545e861bd28f87ea4184aa6
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2023-01-18 20:08:07 -0500

    init project

---
+++ MAPD724-W2023-ICE-Yachao/GameScene.swift
@@ -0,0 +1,50 @@
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene
+{
+    
+    override func sceneDidLoad()
+    {
+   
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+      
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+     
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+      
+    }
+}
