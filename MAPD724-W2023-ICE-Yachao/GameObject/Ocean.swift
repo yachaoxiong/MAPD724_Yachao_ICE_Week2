8c615b34ba1e9903ecd78a06fd240acc474db1a1
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2023-01-18 21:15:01 -0500

    added ocean class

---
+++ MAPD724-W2023-ICE-Yachao/GameObject/Ocean.swift
@@ -0,0 +1,46 @@
+import GameplayKit
+import SpriteKit
+
+
+class Ocean : GameObject
+{
+     // constructor  / initializer
+    init()
+    {
+        super.init(imageString: "ocean", initialScale: 2.0)
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented!")
+    }
+    
+    override func Start()
+    {
+        zPosition = 0
+        verticalSpeed = 5.0
+    }
+    
+    override func Update()
+    {
+        Move()
+        CheckBounds()
+    }
+    override func CheckBounds()
+    {
+        if(position.y <= -773)
+        {
+            Reset()
+        }
+    }
+    
+    override func Reset()
+    {
+        position.y = 773
+    }
+    
+    func Move()
+    {
+        position.y -= verticalSpeed!
+    }
+}
