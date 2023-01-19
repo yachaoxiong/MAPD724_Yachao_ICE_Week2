3ea300d193384b5b32af6e501dca815bf977c6d3
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2023-01-18 20:19:44 -0500

    added my asset

--- MAPD724-W2023-ICE-Yachao/GameScene.swift
+++ MAPD724-W2023-ICE-Yachao/GameScene.swift
@@ -6,20 +6,23 @@ class GameScene: SKScene
     
     override func sceneDidLoad()
     {
-   
+        print("Game Scene Loaded")
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-    
+    func touchDown(atPoint pos : CGPoint)
+    {
+        print("Touch down")
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-      
+    func touchMoved(toPoint pos : CGPoint)
+    {
+        print("Touch moved")
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-     
+    func touchUp(atPoint pos : CGPoint)
+    {
+        print("Touch Up")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
